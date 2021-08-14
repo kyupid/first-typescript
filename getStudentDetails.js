090ab342f94e06a964d6cf50bcca43876a601d20
kyupid <iamscvv@gmail.com>
2021-08-14 23:50:26 +0900

    Set function return type

---
+++ getStudentDetails.js
@@ -0,0 +1,6 @@
+"use strict";
+var age = 21;
+var studentName = 'kyu';
+function getStudentDetails(studentName) {
+    return null;
+}
---
+++ getStudentDetails.ts
@@ -0,0 +1,9 @@
+let age: number = 21;
+let studentName: string = 'kyu';
+
+function getStudentDetails(studentName: string): {
+    age: number;
+    studentName: string;
+} {
+    return null;
+}
